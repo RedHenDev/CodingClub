7bd21517ccd663bee78912b84d902d45b67a3cef
Brian New <neobn@hotmail.com>
2019-10-02 00:09:27 +0100

    Even slower animation now...

--- unRenard.js
+++ unRenard.js
@@ -30,11 +30,11 @@ function draw(){
   background(100,55,20);
   
   if (goingDown == true){
-  cY = cY + 1;
+  cY = cY + 0.1;
   }
   
   if (goingDown == false){
-    cY = cY - 1;
+    cY = cY - 0.1;
   }
   
   // If too low...
